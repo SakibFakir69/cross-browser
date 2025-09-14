8cb0da2eb4cc4adb010e48735f185ab86109b295
Sakib Fakir <FakirSakib22232@gmail.com>
2025-09-14 18:59:26 +0600

    polyfill add

--- index.js
+++ index.js
@@ -3,3 +3,14 @@
 
 // babel convert es6 to es5 
 // babel handel cross browser compatibility 
+// Add polyfills for features like Promise, fetch, Array.from that don’t exist in old browsers.
+// add polyfills cdn for this
+
+// <!-- Polyfill for Promise -->
+// <script src="https://cdn.jsdelivr.net/npm/promise-polyfill/dist/polyfill.min.js"></script>
+
+// <!-- Polyfill for fetch -->
+// <script src="https://cdn.jsdelivr.net/npm/whatwg-fetch/dist/fetch.umd.min.js"></script>
+
+// <!-- Polyfill for Array.from -->
+// <script src="https://cdn.jsdelivr.net/npm/es6-shim/es6-shim.min.js"></script>
